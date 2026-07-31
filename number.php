d2d3854767f828fe35cdf1baaaf845f81c147e89
apriani1236ur <aprilyanianggraini3@gmail.com>
2026-07-31 08:09:50 +0700

    Update number.php

--- number.php
+++ number.php
@@ -1,16 +1,9 @@
 <?php
-// Integer (Bilangan Bulat)
 $umur = 20;
-
-// Float / Double (Bilangan Desimal)
 $ipk = 3.85;
-
-// Menampilkan nilai
 echo "Umur: " . $umur . "<br>";
 echo "IPK: " . $ipk . "<br>";
-
-// Mengecek tipe data menggunakan var_dump
-var_dump($umur); // outputs: int(20)
+var_dump($umur);
 echo "<br>";
-var_dump($ipk);  // outputs: float(3.85)
+var_dump($ipk);  
 ?>
