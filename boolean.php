7b68b82295e997d095db4152b66d400f5869eddd
apriani1236ur <aprilyanianggraini3@gmail.com>
2026-07-31 08:08:58 +0700

    Update boolean.php

--- boolean.php
+++ boolean.php
@@ -1,16 +1,13 @@
 <?php
 $isLulus = true;
 $isGraduated = false;
-
-// Contoh penggunaan pada pengondisian (if-else)
 if ($isLulus) {
     echo "Selamat, Anda dinyatakan Lulus!<br>";
 } else {
     echo "Maaf, Anda tidak lulus.<br>";
 }
 
-// Mengecek tipe data
-var_dump($isLulus);      // outputs: bool(true)
+var_dump($isLulus);     
 echo "<br>";
-var_dump($isGraduated);  // outputs: bool(false)
+var_dump($isGraduated);  
 ?>
